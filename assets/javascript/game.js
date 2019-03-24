c90f70156699bc62d7a44cc4dc64db729e245ae3
cuongle30 <cuongle30@gmail.com>
2019-03-24 15:16:44 -0700

    gem

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,7 +9,9 @@ $(document).ready(function() {
     var crystal3= Math.floor(Math.random()*12+1);
     var crystal4= Math.floor(Math.random()*12+1);
     var totalNumber= 0;
-
+    var gemPlay = document.getElementById("gem-sound");
+    var winPlay = document.getElementById("win-sound");
+    var losePlay = document.getElementById("lose-sound");
 //Random Number between 19 - 120
     let randomNumber = Math.floor((Math.random() *120 ) +19);
     $("#random-number").html(randomNumber)
@@ -29,56 +31,129 @@ $(document).ready(function() {
 
 $("#emerald").on('click', function() {
 //add to current number
+    var gemSound = true;
+    if (gemSound) {
+        gemPlay.pause();
+        gemPlay.currentTime = 0;
+        gemPlay.play();  
+    }
     totalNumber = totalNumber + crystal1;
     $("#total").html(totalNumber)
     console.log(totalNumber)
 // Win or Lose condition. Add score to html
     if (randomNumber === totalNumber) {
         wins++;
+        var winSound = true;
+        if (winSound) {
+        winPlay.pause();
+        winPlay.currentTime = 0;
+        winPlay.play();
+        }
         $(".win").text(`Wins: ${wins}`);
         reset();
     } else if (randomNumber < totalNumber) {
-        losses++
+        losses++;
+        var loseSound = true;
+        if (loseSound) {
+        losePlay.pause();
+        losePlay.currentTime = 0;
+        losePlay.play();
+        }
         $(".lose").text(`Lose: ${losses}`)
         reset();
+        
     }
 })
 $("#diamond").on('click', function() {
+    var gemSound = true;
+    if (gemSound) {
+        gemPlay.pause();
+        gemPlay.currentTime = 0;
+        gemPlay.play();
+    }
     totalNumber = totalNumber + crystal2;
     $("#total").html(totalNumber)
     console.log(totalNumber)
     if (randomNumber === totalNumber) {
         wins++;
         $(".win").text(`Wins: ${wins}`);
+        var winSound = true;
+        if (winSound) {
+        winPlay.pause();
+        winPlay.currentTime = 0;
+        winPlay.play();
+        }
         reset();
     } else if (randomNumber < totalNumber) {
-        losses++
+        losses++;
+        var loseSound = true;
+        if (loseSound) {
+        losePlay.pause();
+        losePlay.currentTime = 0;
+        losePlay.play();
+        }
         $(".lose").text(`Lose: ${losses}`)
         reset();
 }});
 $("#longdiamond").on('click', function() {
+    var gemSound = true;
+    if (gemSound) {
+        gemPlay.pause();
+        gemPlay.currentTime = 0;
+        gemPlay.play();
+    }
     totalNumber = totalNumber + crystal3;
     $("#total").html(totalNumber)
     console.log(totalNumber)
     if (randomNumber === totalNumber) {
         wins++;
+        var winSound = true;
+        if (winSound) {
+        winPlay.pause();
+        winPlay.currentTime = 0;
+        winPlay.play();
+        }
         $(".win").text(`Wins: ${wins}`);
         reset();
     } else if (randomNumber < totalNumber) {
-        losses++
+        losses++;
+        var loseSound = true;
+        if (loseSound) {
+        losePlay.pause();
+        losePlay.currentTime = 0;
+        losePlay.play();
+        }
         $(".lose").text(`Lose: ${losses}`)
         reset();
 }});
 $("#square").on('click', function() {
+    var gemSound = true;
+    if (gemSound) {
+        gemPlay.pause();
+        gemPlay.currentTime = 0;
+        gemPlay.play();
+    }
     totalNumber = totalNumber + crystal4;
     console.log(totalNumber)
     $("#total").html(totalNumber)
     if (randomNumber === totalNumber) {
         wins++;
+        var winSound = true;
+        if (winSound) {
+        winPlay.pause();
+        winPlay.currentTime = 0;
+        winPlay.play();
+        }
         $(".win").text(`Wins: ${wins}`);
         reset();
     } else if (randomNumber < totalNumber) {
-        losses++
+        losses++;
+        var loseSound = true;
+        if (loseSound) {
+        losePlay.pause();
+        losePlay.currentTime = 0;
+        losePlay.play();
+        }
         $(".lose").text(`Lose: ${losses}`)
         reset();
 }});
@@ -86,8 +161,6 @@ $("#square").on('click', function() {
 
 });
     
-
-
 
     
      
